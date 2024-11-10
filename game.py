2940eedbcfbdcec361c2f16af317d8277540ba50
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-11-09 17:30:59 -0800

    Update game.py

--- game.py
+++ game.py
@@ -4,3 +4,7 @@ from pygame.locals import *
 pygame.init()
 HEIGHT, WIDTH = 500
 GRAVITY = -9.8
+FramePerSec = pygame.time.Clock()
+ 
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
